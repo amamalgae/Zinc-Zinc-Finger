a919eb4e1d798496da5310b8a90c5fe1ef2ff416
amamalgae <216372907+amamalgae@users.noreply.github.com>
2026-08-17 14:19:19 +0900

    Bump displayed version for overview alignment fix

--- src/app-version.ts
+++ src/app-version.ts
@@ -1,3 +1,3 @@
-export const APP_VERSION_NUMBER = 32;
+export const APP_VERSION_NUMBER = 33;
 export const APP_VERSION = `ver.${APP_VERSION_NUMBER} (PR #${APP_VERSION_NUMBER})`;
 export const APP_VERSION_PR_URL = "https://github.com/amamalgae/Zinc-Zinc-Finger";
