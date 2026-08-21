8f6192f1ad7499a2b61324a9437441e8f9204d30
amamalgae <216372907+amamalgae@users.noreply.github.com>
2026-08-21 16:59:41 +0900

    Bump app version to 74

--- src/app-version.ts
+++ src/app-version.ts
@@ -1,3 +1,3 @@
-export const APP_VERSION_NUMBER = 73;
+export const APP_VERSION_NUMBER = 74;
 export const APP_VERSION = `ver.${APP_VERSION_NUMBER} (PR #${APP_VERSION_NUMBER})`;
-export const APP_VERSION_PR_URL = "https://github.com/amamalgae/Zinc-Zinc-Finger/pull/73";
+export const APP_VERSION_PR_URL = "https://github.com/amamalgae/Zinc-Zinc-Finger/pull/74";
